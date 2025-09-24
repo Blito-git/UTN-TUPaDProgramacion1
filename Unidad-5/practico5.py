9d1575065f7af2f0848de67a15cc43402e32e14f
Pablo Nogueira <pablonogueirak61@gmail.com>
2025-09-23 21:06:13 -0300

    Subo practica unidad 5

---
+++ Unidad-5/practico5.py
@@ -0,0 +1,42 @@
+print("Ejercicio 1")
+multiplos = list(range(4, 101, 4))
+print(multiplos)
+print("\nEjercicio 2")
+lista = ["pizza", "helado", "mate", "café", "torta"]
+print(lista[-2])
+print("\nEjercicio 3")
+lista = []
+lista.append("hola")
+lista.append("mundo")
+lista.append("python")
+print(lista)
+print("\nEjercicio 4")
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"     
+animales[-1] = "oso"     
+print(animales)
+print("\nEjercicio 5")
+print("el programa busca el número más grande en la lista y lo elimina")
+print("\nEjercicio 6")
+numeros = list(range(10, 31, 5))
+print(numeros[:2])
+print("\nEjercicio 7")
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "golf"
+autos[2] = "vento"
+print(autos)
+print("\nEjercicio 8")
+dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+print("\nEjercicio 9")
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+print("\nEjercicio 10")
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
